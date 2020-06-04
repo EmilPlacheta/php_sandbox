fe4620e84b9ea3191cf16db0990a1bb347b33dc9
Emil Placheta <emil.minoru@gmail.com>
2020-06-04 17:04:04 +0100

    variable definition

--- variables.php
+++ variables.php
@@ -1,5 +1,50 @@
 <?php
+ /* -use ; EVERYWHERE
+    -prefix variables with $
+    -start with letter or _
+    -variables declared with snake_case
+ */
+$my_value = "\nvalue";
+//echo or printo to display, echo is quicker
+echo $my_value;
+print $my_value;
+echo "<br />";
 
-  echo "testy test";
+/* CONSTANTS  */
+define('CONSTANTS', "\nConstants are...constant. Use when value will not change.");
+echo CONSTANTS;
+echo "<br />";
+/* DATA TYPES*/
+
+$string = "\nstring";
+$integer = 1;
+$floats = 1.1;
+$bool = true;
+$arrays = array(1,2,3,4);
+null;
+echo "<br />";
+
+//concat with . or "" and {}
+  $val1 = 'Hello';
+  $val2 = 'World';
+  $hello_world = "$val1 $val2";
+  
+  echo $val1 . $val2;
+  echo "<br />";
+
+  echo $hello_world;
+  echo "<br />";
+  
+  $hello_world2 = "${val1} ${val2}";
+  echo $hello_world2;
+  echo "<br />";
+
+  //escape with backslash \
+
+  $escape = 'That\'s how you escape character';
+  echo $escape;
+
+
+
 
 ?>
